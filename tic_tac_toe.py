da8366ced6b38286b993fd3acafee34e111749f8
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2018-10-10 22:19:44 +0300

    update rendering

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,38 +1,28 @@
 model = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
+    [None, None, None],
+    [None, None, None],
+    [None, None, None],
 ]
 
 
 def view():
     for row in model:
         for column in row:
-            print(f'{column}|', end='')
+            val = column if column else ' '
+
+            print(f'{val}|', end='')
         print()
 
 
-#
-# def view():
-#     s = f'''\
-# {model[0][0]}|{model[0][1]}|{model[0][2]}
-# _____
-# {model[1][0]}|{model[1][1]}|{model[1][2]}
-# _____
-# {model[2][0]}|{model[2][1]}|{model[2][2]}
-#     '''
-#
-#     print(s)
-#
-#
 def get_player_input():
     row = int(input('select row(1-3):')) - 1
     cell = int(input('select cell(1-3):')) - 1
 
     if row > 2 or cell > 2:
+        print('value out of range')
         return get_player_input()
-    else:
-        return row, cell
+
+    return row, cell
 
 
 def update():
@@ -40,10 +30,20 @@ def update():
 
     while True:
         row, cell = get_player_input()
+
+        while model[row][cell]:
+            print('cell already occupied')
+            row, cell = get_player_input()
+
         model[row][cell] = 'X'
         view()
 
         row, cell = get_player_input()
+
+        while model[row][cell]:
+            print('cell already occupied')
+            row, cell = get_player_input()
+
         model[row][cell] = 'O'
         view()
 
