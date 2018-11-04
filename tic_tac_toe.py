66b94be847934eee4bc98e1787cc53fdcb566a62
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2018-11-04 18:47:32 +0200

    update logic

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -7,7 +7,7 @@ model = [
 
 
 def view():
-    for rowIndex, row in enumerate(model):
+    for row_index, row in enumerate(model):
         for index, column in enumerate(row):
             val = column if column else ' '
 
@@ -15,7 +15,7 @@ def view():
 
             if index != 2:
                 print('|', end='')
-        if rowIndex != 2:
+        if row_index != 2:
             print('\n-----')
 
 
@@ -30,13 +30,45 @@ def get_player_input():
     return row, cell
 
 
-def check_is_game_over():
+def find_filled_columns():
+    for index, row in enumerate(model):
+        column_values = [row[index] for row in model]
+        # print(column_values)
+
+        if all(value == 'X' for value in column_values):
+            print('\nplayer one won')
+            return True
+        elif all(value == 'O' for value in column_values):
+            print('\nplayer two won')
+            return True
+
+
+def find_filled_rows():
     for row in model:
+        # no need to check all row values if there's still empty slot
         if None in row:
             return False
 
-    print('GAME OVER.')
-    return True
+        if all(cell_value == 'X' for cell_value in row):
+            print('\n\nPlayer one won')
+            return True
+        elif all(cell_value == 'O' for cell_value in row):
+            print('\n\nPlayer two won')
+            return True
+
+    return False
+
+
+def find_filled_axis():
+    pass
+
+
+def check_is_game_over():
+    if find_filled_rows() or find_filled_columns() or find_filled_axis():
+        print('\nGAME OVER.')
+        return True
+
+    return False
 
 
 def update():
@@ -47,8 +79,7 @@ def update():
             break
 
         # TODO: refactor into a helper function
-        print('\n')
-        print('First player turn')
+        print('\nFirst player turn')
         row, cell = get_player_input()
 
         while model[row][cell]:
@@ -61,8 +92,7 @@ def update():
         if check_is_game_over():
             break
 
-        print('\n')
-        print('Second player turn')
+        print('\nSecond player turn')
         row, cell = get_player_input()
 
         while model[row][cell]:
