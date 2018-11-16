1fa1e87a33bc1325bacb760fcc78a13656f0e0f2
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2018-11-16 16:59:35 +0200

    add comment

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ MODEL = [
 
 
 # TODO maybe return a new model instance from here (without mutating it)
+# def set_model(prev_model, row, cell, value):
 def set_model(row, cell, value):
     MODEL[row][cell] = value
 
