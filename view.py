1fad07866125d58f3ab2f497f53b4292ddf4be69
Dmitriy Kaminskiy <dimaroxx@gmail.com>
2018-11-16 17:08:42 +0200

    refactor view rendering

--- view.py
+++ view.py
@@ -1,20 +1,30 @@
 import os
 
 
-# TODO
-def get_view_template(model): pass
+def get_view(model):
+    return f'''
+    {model[0][0] or ' '}|{model[0][1] or ' '}|{model[0][2] or ' '}
+    _____
+    {model[1][0] or ' '}|{model[1][1] or ' '}|{model[1][2] or ' '}
+    _____
+    {model[2][0] or ' '}|{model[2][1] or ' '}|{model[2][2] or ' '}
+    '''
 
 
 def render(model):
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    for row_index, row in enumerate(model):
-        for index, column in enumerate(row):
-            val = column if column else ' '
+    view = get_view(model)
 
-            print(f'{val}', end='')
+    print(view)
 
-            if index != 2:
-                print('|', end='')
-        if row_index != 2:
-            print('\n-----')
+    # for row_index, row in enumerate(model):
+    #     for index, column in enumerate(row):
+    #         val = column if column else ' '
+    #
+    #         print(f'{val}', end='')
+    #
+    #         if index != 2:
+    #             print('|', end='')
+    #     if row_index != 2:
+    #         print('\n-----')
